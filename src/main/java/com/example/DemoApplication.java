f6fe0f71198c2aa40dd29e353ca7201c1666a584
Tyrone Croswell <tcroswell@gmail.com>
2015-12-06 18:26:53 -0500

    Initial commit

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/*@EnableAutoConfiguration
+@Configuration
+@ComponentScan*/
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DemoApplication.class, args);
+
+    }
+}
---
+++ src/main/java/com/example/User.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * Created by croswet on 12/5/2015.
+ */
+public class User {
+    public long id;
+    public String name;
+    public String alias;
+
+    public User(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
